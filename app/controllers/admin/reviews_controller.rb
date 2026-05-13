e881e141d976df5174f9debfdc1b9904017641bd
vandorena <alexvd@hackclub.com>
2026-05-13 16:02:50 -0400

    basic skeleton of the goi dash

--- app/controllers/admin/reviews_controller.rb
+++ app/controllers/admin/reviews_controller.rb
@@ -3,7 +3,10 @@ module Admin
     def index
       authorize :admin, :access_reviews?
 
-      @reviews = YswsReview.all
+      @reviews = YswsReview
+        .where(reviewed_at: nil)
+        .includes(:project, :user)
+        .order(created_at: :asc)
     end
   end
 end
